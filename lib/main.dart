55ee774c5fc40917ae772aa6c90facf27d5f25c7
runnerro4d <aaditya.n.sinha@gmail.com>
2018-12-07 02:26:22 +1100

    Using Flutter Material Components

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyScaffold extends StatelessWidget{
         children: <Widget>[
           MyAppBar(
             title: Text(
-              'Example Title',
+              'aahhh Title',
               style: Theme.of(context).primaryTextTheme.title,
       ),
     ),
@@ -61,7 +61,40 @@ class MyScaffold extends StatelessWidget{
 
 void main(){
   runApp(MaterialApp(
-    title: 'My app',
-    home: MyScaffold(),
+    title: 'Flutter App',
+    home: TutorialHome(),
   ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
 }
