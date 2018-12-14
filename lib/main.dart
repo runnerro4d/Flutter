b0ffade5513810a9d1fd55875517fea85b7fdc46
runnerro4d <aaditya.n.sinha@gmail.com>
2018-12-14 21:43:02 +1100

    initial stuff

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.deepPurple,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rect'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
